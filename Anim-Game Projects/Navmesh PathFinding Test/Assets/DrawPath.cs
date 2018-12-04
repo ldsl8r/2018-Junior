aeb8bef2c4d0080f64964033f2db380d81c0e665
Logan Slater <lslater742@gmail.com>
2018-12-04 15:13:18 -0700

    Reimported WHitebox to show progress, worked on path code with matt

--- Anim-Game Projects/Navmesh PathFinding Test/Assets/DrawPath.cs
+++ Anim-Game Projects/Navmesh PathFinding Test/Assets/DrawPath.cs
@@ -6,43 +6,52 @@ using UnityEngine.AI;
 public class DrawPath : MonoBehaviour 
 {
     private LineRenderer lr;
-    private NavMeshPath path;
+    public List<NavMeshPath> paths;
 
     public List<Transform> waypoints;
 
     void Start()
     {
         lr = GetComponent<LineRenderer>();
-        path = new NavMeshPath();
+        foreach(Transform waypoint in waypoints)
+        {
+            NavMeshPath path = new NavMeshPath();
+            paths.Add(path);
+        }
 
         StartCoroutine(Begin());
     }
 
-    IEnumerator Begin()
+    void DrawPaths(List<NavMeshPath> paths)
     {
-        while(true)
+        int corner = 1;
+        foreach(NavMeshPath path in paths)
         {
-            for (int i = 0; i < waypoints.Capacity - 1; i++)
-            {
-                NavMesh.CalculatePath(waypoints[i].position, waypoints[i+1].position, NavMesh.AllAreas, path);
+            corner += path.corners.Length;
+        }
 
-                yield return new WaitForEndOfFrame();
-                DrawPaths(path);
+        if(corner >= 2)
+        {
+            lr.positionCount = corner +1;
+
+            for(int i=0; i < paths.Capacity; i++)
+            {
+                for (int y = 0; y < paths[i].corners.Length; y++)
+                {
+                    lr.SetPosition(i+y, paths[i].corners[y]);
+                }
             }
-            yield return new WaitForFixedUpdate();
         }
     }
 
-    void DrawPaths(NavMeshPath path)
+    IEnumerator Begin()
     {
-        if(path.corners.Length >= 2)
+        for (int i = 0; i < waypoints.Capacity - 1; i++)
         {
-            lr.positionCount = path.corners.Length +1;
+            NavMesh.CalculatePath(waypoints[i].position, waypoints[i+1].position, NavMesh.AllAreas, paths[i]);
 
-            for(int i=0; i < path.corners.Length; i++)
-            {
-                lr.SetPosition(i, path.corners[i]);
-            }
+            yield return new WaitForEndOfFrame();
         }
+        DrawPaths(paths);    
     }
 }
