d69917657796f04bf28c7c8a2fd9d1a5fcbd8fb6
Logan Slater <lslater742@gmail.com>
2018-12-04 13:15:04 -0700

    worked on scripts a little bit, Worked on dragon, worked on whisper rig

--- Scripting/Python Scripts/RandomPlacement.py
+++ Scripting/Python Scripts/RandomPlacement.py
@@ -1,24 +1,51 @@
 import maya.cmds as cmds
 import random
-def RandomObjects(NumofDuplicates, XrangeMin, XrangeMax, YrangeMin, YrangeMax, ZrangeMin, ZrangeMax):
 
-    Objects=[]
-    sels = cmds.ls(sl=True)
 
-    i=0
-    while (i<NumofDuplicates):
-        tempsort =cmds.duplicate(sels[0],rr=True)
-        Objects.append(tempsort[0])
-        i+=1
+class RandPlace:
+    def __init__(self):
+        pass
 
-    for moveStuff in Objects:
+    def RandomObjects(self, NumofDuplicates, XrangeMin, XrangeMax, YrangeMin, YrangeMax, ZrangeMin, ZrangeMax):
 
-        xpos = random.randrange (XrangeMin, XrangeMax)
-        ypos = random.randrange (YrangeMin, YrangeMax)
-        zpos = random.randrange (ZrangeMin, ZrangeMax)
-        cmds.move(xpos, ypos, zpos, moveStuff)
+        Objects=[]
+        sels = cmds.ls(sl=True)
 
-RandomObjects(8,-9,9,-7,7,-5,5)
+        i=0
+        while (i<NumofDuplicates):
+            tempsort =cmds.duplicate(sels[0],rr=True)
+            Objects.append(tempsort[0])
+            i+=1
+
+        for moveStuff in Objects:
+
+            xpos = random.randrange (XrangeMin, XrangeMax)
+            ypos = random.randrange (YrangeMin, YrangeMax)
+            zpos = random.randrange (ZrangeMin, ZrangeMax)
+            cmds.move(xpos, ypos, zpos, moveStuff)
+
+    def CreateRandUI(self):
+
+        self.mWindow = cmds.window(self.mWindow, title='Duplicate and Random Placement')
+        self.mCol = cmds.columnLayout(parent=self.mWindow, adjustableColumn=True)
+        self.nameField = cmds.textField(placeholderText='Enter new name')
+
+        self.dupRowLayout = cmds.rowLayout(parent=self.mCol, numberOfColumns=2, adjustableColumn2=True)
+        cmds.text(parent=self.dupRowLayout, label="Duplicate")
+        self.duplicateIntField =cmds.intField(parent=self.dupRowLayout, minValue=1, value=7)
+
+        self.rangeRowLayout = cmds.rowLayout(parent=self.mCol, numberOfColumns=4)
+        cmds.text(parent=self.rangeRowLayout, label="Min Range")
+        cmds.minxRange(cmds.floatField, parent=self.rangeRowLayout, value=0)
+        cmds.minyRange(cmds.floatField, parent=self.rangeRowLayout, value=0)
+        cmds.minzRange(cmds.floatField, parent=self.rangeRowLayout, value=0)
+
+        self.rangeRowLayout = cmds.rowLayout(parent=self.mCol, numberOfColumns=4)
+        cmds.text(parent=self.rangeRowLayout,label="MaxRange")
+        cmds.maxxRange(cmds.floatField, parent=self.rangeRowLayout, value=0)
+        cmds.maxyRange(cmds.floatField, parent=self.rangeRowLayout, value=0)
+        cmds.maxzRange(cmds.floatField, parent=self.rangeRowLayout, value=0)
+
 
 
 
