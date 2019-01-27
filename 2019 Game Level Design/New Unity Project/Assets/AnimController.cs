4cf44a59cfebcc5d2e9b38f44bdb2d3abab46f35
Logan Slater <lslater742@gmail.com>
2019-01-27 16:57:42 -0700

    Worked on Level Design Unity Project, Buggy a little, and lagoon project

---
+++ 2019 Game Level Design/New Unity Project/Assets/AnimController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimController : MonoBehaviour {
+
+public Animator anim;
+
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("f"))
+		{
+			anim.Play("Explode");
+		}
+		if(Input.GetKeyDown("g"))
+		{
+			anim.Play("MoveSarc");
+		}
+	}
+}
