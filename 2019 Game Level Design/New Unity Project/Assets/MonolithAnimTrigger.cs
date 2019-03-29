20ca17b8de1d3872ee30a767e8992238ff21b909
Logan <lslater742@gmail.com>
2019-03-29 10:34:35 -0600

    worked on level UI and got anims working right, imported water shader

---
+++ 2019 Game Level Design/New Unity Project/Assets/MonolithAnimTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonolithAnimTrigger : MonoBehaviour {
+    public Animator anim;
+    int state = 0; 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag ==  "Pickup")
+        {
+            state++;
+            anim.SetInteger("Stage", state);
+        }
+    }
+}
