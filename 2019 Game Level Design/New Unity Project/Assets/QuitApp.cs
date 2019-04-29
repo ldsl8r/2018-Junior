37f7a6640026ab764d9156337126d831c728d485
Logan Slater <lslater742@gmail.com>
2019-04-29 16:49:29 -0600

    Worked on level design UI, ready to test

---
+++ 2019 Game Level Design/New Unity Project/Assets/QuitApp.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitApp : MonoBehaviour {
+
+	 void Update()
+    {
+        if (Input.GetKey("I"))
+        {
+            Application.Quit();
+        }
+    }
+}
